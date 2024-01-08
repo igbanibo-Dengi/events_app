659c897bbb95dac929c42e583b27557354d97ec5
igbanibo-Dengi <igbanibodengi@gmail.com>
2024-01-08 21:56:58 +0100

    completed nav bar

--- components/ui/shared/Header.tsx
+++ components/ui/shared/Header.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Button } from "../button";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
@@ -16,9 +18,15 @@ const Header = () => {
             alt="logo"
           />
         </Link>
+        <SignedIn>
+          <nav className="md:flex hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
+            <MobileNav />
           </SignedIn>
           <SignedOut>
             <Button asChild size={"lg"}>
