646ce1f9809b2ab5f0814ee71a41dae51ce5d6d7
igbanibo-Dengi <igbanibodengi@gmail.com>
2024-01-08 22:34:21 +0100

    Completed Hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,44 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-4xl">Events</h1>
-    </main>
+    <>
+      <section className="bg-primary/20 bg-dotted-pattern bg-contain py-5 md:py-10">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">
+              Host, Connect, Celebrate: Your Events, \our Platform!
+            </h1>
+            <p className="p-regular-20 md:p-regular-24">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusantium sint harum obcaecati odio, quaerat
+            </p>
+            <Button className="w-full sm:w-fit" size={"lg"} asChild>
+              <Link href="#events">Expore Now</Link>
+            </Button>
+          </div>
+          <Image
+            src="/assets/images/hero.png"
+            alt="Hero"
+            width={1000}
+            height={1000}
+            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+          />
+        </div>
+      </section>
+      <section
+        id="events"
+        className="wrapper my-8 flex flex-col gap-8 md:gap-12"
+      >
+        <h2 className="h2-bold">
+          Lorem, ipsum dolor sit amet <br /> elit. Minus, dicta?
+        </h2>
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          search CategoryFilter
+        </div>
+      </section>
+    </>
   );
 }
--- components/ui/shared/Footer.tsx
+++ components/ui/shared/Footer.tsx
@@ -1,7 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <footer className="border-t">
+        <div className="flex-center wrapper flex-between flex-col gap-4 p-5 text-center sm:flex-row">
+          <Link href="/">
+            <Image
+              src="/assets/images/logo.svg"
+              alt="logo"
+              width={128}
+              height={38}
+            />
+          </Link>
+          <p>2023 dionysus. All rights reserved</p>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
