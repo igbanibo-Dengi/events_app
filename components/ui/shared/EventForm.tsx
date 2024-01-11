29e8dd96b9b3d873b7c4999520efc149fd6d717d
igbanibo-Dengi <igbanibodengi@gmail.com>
2024-01-11 23:15:04 +0100

    event pages and mongo db fix

---
+++ components/ui/shared/EventForm.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+type EventFormProps = {
+  userId: String;
+  type: "Create" | "Update";
+};
+
+const EventForm = ({ userId, type }: EventFormProps) => {
+  return <div>EventForm {type}</div>;
+};
+
+export default EventForm;
