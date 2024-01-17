6ea6e4424ec2564e3c2fe59ea1506223fe7587b6
igbanibo-Dengi <igbanibodengi@gmail.com>
2024-01-17 04:04:17 +0100

    added loading animation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,7 @@
 import Footer from "@/components/ui/shared/Footer";
 import Header from "@/components/ui/shared/Header";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 export default function RootLayout({
   children,
@@ -9,7 +11,9 @@ export default function RootLayout({
   return (
     <div className="flex h-screen flex-col">
       <Header />
-      <main className="flex-1 ">{children}</main>
+      <Suspense fallback={<Loading />}>
+        <main className="flex-1 ">{children}</main>
+      </Suspense>
       <Footer />
     </div>
   );
